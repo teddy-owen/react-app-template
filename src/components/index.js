fd688d6cdd42e3e9f1c201d58cf0b11ec8894733
Teddy Owen <teddyeowen@gmail.com>
2020-04-20 10:33:27 -0400

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,22 @@ export {CustomButton, TransparentButton, PrimaryButton, CircleButton} from "./bu
 export {CustomInput,EmailInput,PasswordInput,InputErrorMessage, CustomTextArea} from "./inputs.component.js";
 export {Loader} from "./loader.component.js";
 export {ConfirmDialog, InputDialog} from "./dialogs.component.js";
+export {
+    FlexBox,
+    FlexRowBetweenStretch,
+    FlexColumnBetweenStretch,
+    FlexRowBetweenCenter,
+    FlexColumnBetweenCenter,
+    FlexRowStartStretch,
+    FlexColumnStartStretch,
+    FlexRowStartCenter,
+    FlexColumnStartCenter,
+    FlexRowEndStretch,
+    FlexColumnEndStretch,
+    FlexRowEndCenter,
+    FlexColumnEndCenter,
+    FlexRowAroundStretch,
+    FlexColumnAroundStretch,
+    FlexRowAroundCenter,
+    FlexColumnAroundCenter,
+} from "./flex.component.js";
