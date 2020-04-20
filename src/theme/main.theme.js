aa6f577851874047a558f3b3dfc740f41c694f79
Teddy Owen <teddyeowen@gmail.com>
2020-04-20 09:46:13 -0400

    Update main.theme.js

--- src/theme/main.theme.js
+++ src/theme/main.theme.js
@@ -3,7 +3,8 @@ class Theme{
     static secondaryFontFamily="'Roboto', sans-serif";
     static primaryButtonColor="#FF6978"; 
     static buttonFontFamily="'Arial'";
-    static color1="#6515E4";
+    static color1="#273469";
+    static color2="#1E2749";
     static dangerColor="#ff2d2c";
     static fontColor1="#4c4c4c";
 }
