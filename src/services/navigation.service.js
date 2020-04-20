a3d706e1ef0e632cb4e8e924222fa238d6814cc4
Teddy Owen <teddyeowen@gmail.com>
2020-04-20 13:13:49 -0400

    Update navigation.service.js

--- src/services/navigation.service.js
+++ src/services/navigation.service.js
@@ -1,5 +1,32 @@
+import queryString from 'query-string';
 
 class Navigation{
+    
+    static getURLParam(props=null,key=""){
+        console.assert(props.match != null, "Must ensure component wrapped in 'withRouter' to allow URL access.");
+        return props.match.params[key];
+    }
+
+    static getQueryObject(props=null){
+        console.assert(props.location != null, "Must ensure component wrapped in 'withRouter' to allow URL access.");
+        let parsedQueryString = queryString.parse(props.location.search);
+        return parsedQueryString;
+    }
+
+    static getQueryParam(props=null,key=""){
+        console.assert(props.location != null, "Must ensure component wrapped in 'withRouter' to allow URL access.");
+        let parsedQueryString = Navigation.getQueryObject(props);
+        return parsedQueryString[key];
+    }
+
+    static setQueryParams(props=null,queryObject=null){
+        console.assert(props.history != null, "Must ensure component wrapped in 'withRouter' to allow URL access.");
+        props.history.push({
+            search:queryString.stringify(queryObject),
+        });
+        return;
+    }
+    
     static paths = {
         root:"/",
         auth:"/auth",
